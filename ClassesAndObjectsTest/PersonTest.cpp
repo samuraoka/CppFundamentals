c45c9001042a95e0fedd7afb81af1ea7bbdcf502
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-03 06:48:19 +0900

    Updated a test.

--- ClassesAndObjectsTest/PersonTest.cpp
+++ ClassesAndObjectsTest/PersonTest.cpp
@@ -8,6 +8,17 @@
 #include "Person.h"
 
 TEST(PersonTest, ShouldCreatePersonObject) {
+	// Arrange
+	// Nothing to do
+
 	// Act
 	Person p;
+
+	// Analyze
+	// Nothing to do
+
+	// Assert
+	// Why is the size of an empty class in C++ not zero? [duplicate]
+	// https://stackoverflow.com/questions/2362097/why-is-the-size-of-an-empty-class-in-c-not-zero
+	EXPECT_EQ(1, sizeof(p));
 }
